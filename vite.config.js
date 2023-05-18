12ab8d71d2d4f5f7bd56af89145c343f006f3076
Muneeb Aslam <muneeb.official.686332@hotmail.com>
2023-05-18 05:49:45 +0500

    Done

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
+// vite.config.js
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: "./"
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        nested: resolve(__dirname, 'src/*.html'),
+      },
+    },
+  },
 })
